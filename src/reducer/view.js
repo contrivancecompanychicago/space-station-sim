1a35a47591ae372da00ac7be0921decebccbda02
James Smith <james@digijin.com>
2016-05-08 22:19:07 +1000

    view reducer

--- src/reducer/view.js
+++ src/reducer/view.js
@@ -8,5 +8,23 @@
 //     x: 0
 //     y: 0
 export default function view(state = {}, action){
-  return state;
+  let out = {};
+  if(action.type === 'VIEW_SCALE'){
+    out.scale = action.scale;
+  }else{
+    out.scale = state.scale || 1;
+  }
+
+  if(action.type === 'VIEW_MOVE'){
+    out.offset = {
+      x: state.offset.x + action.x,
+      y: state.offset.y + action.y
+    }
+  }else if(action.type === 'VIEW_SET_OFFSET'){
+    out.offset = {x:action.x, y:action.y};
+  }else{
+    out.offset = state.offset || {x:0,y:0}
+  }
+
+  return out;
 }
--- test/reducer/view.spec.js
+++ test/reducer/view.spec.js
@@ -5,7 +5,7 @@ describe('reducer/view', () => {
     expect(view(undefined, {type:'NOTHING'}).scale).toBe(1);
   });
   it('should return a x y offset', () => {
-    let out = view(undefined, {type:'NOTHING'})
+    let out = view(undefined, {type:'NOTHING'});
     expect(out.offset).toBeDefined();
     expect(out.offset.x).toBe(0);
     expect(out.offset.y).toBe(0);
@@ -15,12 +15,12 @@ describe('reducer/view', () => {
     expect(out.scale).toBe(5);
   });
   it('should respond to VIEW_MOVE', () => {
-    let out = view({offset:{x:1, y:2}}, {type:'VIEW_MOVE', x:3, y:4});
+    let out = view({scale: 1, offset:{x:1, y:2}}, {type:'VIEW_MOVE', x:3, y:4});
     expect(out.offset.x).toBe(4);
     expect(out.offset.y).toBe(6);
   });
   it('should respond to VIEW_SET_OFFSET', () => {
-    let out = view({offset:{x:1, y:2}}, {type:'VIEW_SET_OFFSET', x:3, y:4});
+    let out = view({scale: 1, offset:{x:1, y:2}}, {type:'VIEW_SET_OFFSET', x:3, y:4});
     expect(out.offset.x).toBe(3);
     expect(out.offset.y).toBe(4);
   });
