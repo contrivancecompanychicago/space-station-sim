5e1cb486146ada371aa2073213d7661d92903521
James Smith <james.smith@mi9.com.au>
2016-05-31 16:01:28 +1000

    basic game

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,11 @@
-
+import Imagine from 'imagine-engine';
 
 export default class Game{
+  constructor(container){
+    this.container = container;
+    this.container.appendChild(document.createElement('canvas'));
+    this.engine = new Imagine();
+    this.manager = this.engine.register({type:'manager', game:this});
+  }
   
 }
--- src/Game.spec.js
+++ src/Game.spec.js
@@ -1,7 +1,26 @@
 import Game from 'Game';
 
 describe('Game', () => {
+  let div;
+  let game;
+  beforeEach(() => {
+    div = document.createElement('div');
+    game = new Game(div);
+  });
+
   it('should be defined', () => {
     expect(Game).toBeDefined();
-  })
+  });
+
+  it('should put some shit into a div', () => {
+    expect(div.children.length > 0).toBe(true);
+  });
+
+  it('should spawn the engine', () => {
+    expect(game.engine).toBeDefined();
+  });
+
+  it('should make the manager object', () => {
+    expect(game.engine.getComponent('manager')).toBeDefined();
+  });
 })
