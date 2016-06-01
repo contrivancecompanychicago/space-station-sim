ed209f7f26e3f7af98e42fa3e5cf6442265c81fc
James Smith <james.smith@mi9.com.au>
2016-06-01 15:15:57 +1000

    grid keys

--- src/Game/GridManager.js
+++ src/Game/GridManager.js
@@ -5,10 +5,11 @@ export default class GridManager{
 
   //private
   makeKey(x, y){
-
+    return `${x}_${y}`;
   }
   parseKey(key){
-
+    let parts = key.split('_');
+    return {x:parseInt(parts[0]), y:parseInt(parts[1])};
   }
 
   addNode(x, y, node){
--- src/Game/GridManager.spec.js
+++ src/Game/GridManager.spec.js
@@ -14,14 +14,14 @@ describe('Game/GridManager', () => {
 
   describe('makeKey', () => {
     it('should turn x, y into string', () => {
-      let key = gridManager.makeKey(12, 34);
+      let key = gridManager.makeKey(-12, 34);
       expect(typeof key).toBe('string');
     });
   });
 
   describe('parseKey', () => {
     it('should turn string from makekey into its orig values', () => {
-      let orig = {x:12, y:34};
+      let orig = {x:-12, y:34};
       let key = gridManager.makeKey(orig.x, orig.y);
       let parsed = gridManager.parseKey(key);
       expect(orig.x).toBe(parsed.x);
