58e82ab66e0f420a534346cf89d4baec96bc46a2
James Smith <james.smith@mi9.com.au>
2016-06-01 14:51:40 +1000

    adding shit to grid

--- src/Game/GridManager.spec.js
+++ src/Game/GridManager.spec.js
@@ -1,4 +1,6 @@
 import GridManager from 'Game/GridManager';
+import { keys } from 'lodash';
+
 let gridManager;
 let state;
 describe('Game/GridManager', () => {
@@ -8,5 +10,21 @@ describe('Game/GridManager', () => {
   });
   it('should capture a ref to state', () => {
     expect(gridManager.state).toBe(state);
+  });
+
+  describe('addNode', () => {
+    it('should add a node', () => {
+      let nodes = keys(state).length;
+      gridManager.addNode(1, 2, "test");
+      expect(keys(state).length - nodes).toBe(1);
+    });
+  });
+
+  describe('getGridOffset', () => {
+    it('should return an object with min/max x/y', () => {
+      let output = gridManager.getGridOffset();
+      expect(output.minx).toBeDefined();
+      expect(output.miny).toBeDefined();
+    });
   });
 });
