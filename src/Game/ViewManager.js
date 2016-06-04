0b40d61a83bcd19afecd4c0bee06406e5dad8cba
James Smith <james@digijin.com>
2016-06-05 01:29:38 +1000

    mousey

--- src/Game/ViewManager.js
+++ src/Game/ViewManager.js
@@ -1,7 +1,20 @@
 
 export default class ViewManager{
+
   constructor(state) {
     this.state = state;
+  }
+
+  start(){
+    this.addListeners();
+  }
+
+  addListeners() {
+    document.addEventListener('mousedown', this.onMouseDown);
+  }
+
+  onMouseDown(e){
+    console.log(e.button);
   }
 }
 
--- src/Game/ViewManager.spec.js
+++ src/Game/ViewManager.spec.js
@@ -1,4 +1,38 @@
 import ViewManager from 'Game/ViewManager';
+import Imagine from 'imagine-engine';
+import { extend } from 'lodash';
+
+let state;
+let viewManager;
+
+//keypresses
+let mouseEvent = function(eventName, params){
+  var event = document.createEvent('Event');
+  extend(event, params);
+  event.initEvent(eventName);
+  document.dispatchEvent(event);
+};
+let middleMouseDown = function(){
+  mouseEvent('mousedown', {button: 1});
+};
+let middleMouseUp = function(){
+  mouseEvent('mouseup', {button: 1});
+};
+
+
 describe('Game/ViewManager', () => {
-  
+  beforeEach(() => {
+    state = {};
+    viewManager = new ViewManager(state);
+  });
+
+  describe('detecting middle mouse', () => {
+    it('should be called by imagine', () =>{
+      spyOn(viewManager, 'onMouseDown').and.callThrough();
+      viewManager.start();
+      middleMouseDown();
+      expect(viewManager.onMouseDown).toHaveBeenCalled();
+    });
+  });
+
 });
