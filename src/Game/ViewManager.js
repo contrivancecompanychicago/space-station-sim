b2639d671b5ff5c1eb016b666eac88bf3d839fae
James Smith <james@digijin.com>
2016-06-05 02:00:09 +1000

    dragging

--- src/Game/ViewManager.js
+++ src/Game/ViewManager.js
@@ -1,8 +1,21 @@
+import {defaults} from 'lodash';
+
+const initial = {
+  scale: 1,
+  offset: {
+    x: 0,
+    y: 0
+  }
+};
 
 export default class ViewManager{
 
   constructor(state) {
-    this.state = state;
+    if(!state){
+      state = {};
+    }
+    this.state = defaults(state, initial);
+    this.dragging = false;
   }
 
   start(){
@@ -10,17 +23,37 @@ export default class ViewManager{
   }
 
   addListeners() {
-    document.addEventListener('mousedown', this.onMouseDown);
+    document.addEventListener('mousedown', this.onMouseDown.bind(this));
+    document.addEventListener('mouseup', this.onMouseUp.bind(this));
+    document.addEventListener('mousemove', this.onMouseMove.bind(this));
   }
 
   onMouseDown(e){
-    console.log(e.button);
+    if(e.button === 1){
+      this.startDrag(e);
+    }
   }
+  onMouseUp(e){
+    if(e.button === 1){
+      this.stopDrag();
+    }
+  }
+  onMouseMove(e) {
+    if(this.dragging){
+      let delta = {x:e.pageX-this.lastPos.x, y: e.pageY-this.lastPos.y};
+      this.lastPos = {x:e.pageX, y: e.pageY};
+      this.state.offset.x += delta.x;
+      this.state.offset.y += delta.y;
+    }
+  }
+
+  startDrag(e){
+    this.dragging = true;
+    this.lastPos = {x:e.pageX, y: e.pageY};
+  }
+
+  stopDrag(){
+    this.dragging = false;
+  }
+
 }
-
-
-	// view:
-	// 	scale: 1
-	// 	offset:
-	// 		x: 0
-	// 		y: 0
--- src/Game/ViewManager.spec.js
+++ src/Game/ViewManager.spec.js
@@ -26,12 +26,46 @@ describe('Game/ViewManager', () => {
     viewManager = new ViewManager(state);
   });
 
+  describe('constructor', () => {
+    it('should set defaults', () => {
+      expect(viewManager.state).toBeDefined();
+      expect(viewManager.state.offset).toBeDefined();
+    });
+    it('should mutate', () => {
+      expect(viewManager.state).toBe(state);
+    });
+  });
+
   describe('detecting middle mouse', () => {
     it('should be called by imagine', () =>{
       spyOn(viewManager, 'onMouseDown').and.callThrough();
+      spyOn(viewManager, 'startDrag').and.callThrough();
+      spyOn(viewManager, 'onMouseUp').and.callThrough();
+      spyOn(viewManager, 'stopDrag').and.callThrough();
       viewManager.start();
       middleMouseDown();
       expect(viewManager.onMouseDown).toHaveBeenCalled();
+      expect(viewManager.startDrag).toHaveBeenCalled();
+      middleMouseUp();
+      expect(viewManager.onMouseUp).toHaveBeenCalled();
+      expect(viewManager.stopDrag).toHaveBeenCalled();
+    });
+  });
+
+  describe('drag', () => {
+    it('should work', () => {
+      viewManager.startDrag({pageX: 0, pageY: 0});
+      viewManager.onMouseMove({pageX: 10, pageY: 20});
+      expect(viewManager.state.offset.x).toBe(10);
+      expect(viewManager.state.offset.y).toBe(20);
+    });
+  });
+
+  describe('startDrag', () => {
+    it('should set lastPos', () => {
+      expect(viewManager.lastPos).not.toBeDefined();
+      viewManager.startDrag({pageX: 0, pageY: 0});
+      expect(viewManager.lastPos).toBeDefined();
     });
   });
 
