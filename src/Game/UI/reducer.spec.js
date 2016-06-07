4b679a1bf504d729b15411668c75e39f50daf89a
James Smith <james.smith@mi9.com.au>
2016-06-07 17:46:40 +1000

    really boring spec

--- src/Game/UI/reducer.spec.js
+++ src/Game/UI/reducer.spec.js
@@ -1,5 +1,13 @@
 import reducer from 'Game/UI/reducer';
 
 describe('Game/UI/reducer', () => {
-
+  describe('should contain', () => {
+    let state = {};
+    beforeEach(()=>{
+      state = reducer({}, {type:'NOTHING'});
+    });
+    it('mode', () => { expect(state.mode).toBeDefined();});
+    it('grid', () => { expect(state.grid).toBeDefined();});
+    it('item', () => { expect(state.item).toBeDefined();});
+  });
 });
