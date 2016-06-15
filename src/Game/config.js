3460ad28aa55bbb9560ad61c2745f494052f647f
James Smith <james@digijin.com>
2016-06-15 21:52:44 +1000

    config

---
+++ src/Game/config.js
@@ -0,0 +1,17 @@
+export default {
+  view: {
+    scale: {
+      max: 5,
+      min: 0.5,
+      step: 0.1
+    }
+  },
+  grid: {
+    width: 32,
+    height: 32,
+    debugtext: false
+  },
+  character:{
+    radius: 10
+  }
+};
