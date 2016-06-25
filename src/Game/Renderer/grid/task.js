e03e6d636856380c63f7c72a2248834c9d6bae18
James Smith <james@digijin.com>
2016-06-25 19:07:20 +1000

    clean up deps

--- src/Game/Renderer/grid/task.js
+++ src/Game/Renderer/grid/task.js
@@ -1,13 +1,4 @@
-import {worldToScreen, blockToPoint} from 'Util';
-import config from 'Game/config';
-
-const blockWidth = config.grid.width;
-const blockHeight = config.grid.height;
-
-import Types from 'Game/Type/Grid';
-
 import {keys} from 'lodash';
-
 
 import renderBlock from './block';
 
