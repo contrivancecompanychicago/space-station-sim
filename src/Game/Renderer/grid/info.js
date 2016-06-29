ed686eaf7d5dbb1326f94a56f7a0b0586ba5ec6b
James Smith <james@digijin.com>
2016-06-29 22:43:11 +1000

    fps

--- src/Game/Renderer/grid/info.js
+++ src/Game/Renderer/grid/info.js
@@ -1,10 +1,48 @@
-import {keys} from 'lodash';
+import {keys, tail} from 'lodash';
+
+//HACK kinda cus I wanted these to be pure, but I think this is a good exception
+class FPS{
+  constructor() {
+    this.count = 0;
+    this.fps = [];
+    setInterval(() => {
+
+      this.fps.push(this.count);
+      this.count = 0;
+      // console.log(this.fps);
+      if(this.fps.length>100){
+        this.fps.shift();
+      }
+
+    }, 1000);
+  }
+  getStats(){
+
+  }
+  render(layer){
+    this.count++;
+    let i = 0;
+    this.fps.forEach((second)=>{
+      i++;
+      layer.context.fillRect(i, 0, i+1, second/2);
+    });
+
+    layer.context.fillStyle = "white";
+    layer.context.fillText(this.fps[this.fps.length-1]+"FPS", 10, 10);
+  }
+}
+
+let fps = new FPS();
 
 export default function renderInfo(state, layer){
+  let offset = 60;
+  let lineHeight = 20;
+  fps.render(layer);
   layer.context.fillStyle = "grey";
   layer.context.font = '14px verdana';
-  layer.context.fillText(JSON.stringify(state.View), 10, 20);
-  layer.context.fillText(keys(state.Grid).length+" grid objects", 10, 40);
-  layer.context.fillText(keys(state.Character).length+" character objects", 10, 60);
-  layer.context.fillText(keys(state.Task).length+" task objects", 10, 80);
+  layer.context.fillText(JSON.stringify(state.View), 10, offset+= lineHeight);
+  layer.context.fillText("x "+state.View.offset.x+" y " + state.View.offset.y, 10, offset+= lineHeight);
+  layer.context.fillText(keys(state.Grid).length+" grid objects", 10, offset+= lineHeight);
+  layer.context.fillText(keys(state.Character).length+" character objects", 10, offset+= lineHeight);
+  layer.context.fillText(keys(state.Task).length+" task objects", 10, offset+= lineHeight);
 }
