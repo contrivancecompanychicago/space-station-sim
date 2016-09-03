72c0e468c60d529c7ddb6f34b4c4a8fc746686a3
digijin <james@digijin.com>
2016-09-04 02:04:48 +1000

    breaking up character

--- src/Game/Manager/Character.js
+++ src/Game/Manager/Character.js
@@ -6,6 +6,7 @@ import config from 'Game/config';
 
 import {blockToPoint, pointToBlock, blockToCenter, pointAtBlock} from 'Util';
 
+import State from './Character/State'
 
 function centerBlock(block){
   return {
@@ -23,48 +24,8 @@ const States = {
   RANDOM: 'random',
   TASK: 'task'
 };
-//start update and end
-const State = {
-  idle:{
-    update: function(char){
-      let task = this.taskManager.getUnassignedTask();
-      if(task){
-        this.taskManager.assignTask(task.id, char.id);
-        char.task = task.id;
-        this.changeState(char, States.TASK);
-      }
-      if(Math.random()<0.01){
-        this.changeState(char, States.RANDOM);
-      }
-    },
-  },
-  random:{
-    start: function(char){
-      this.makePath(char, this.gridManager.randomNode());
-    },
-    update: function(char){
-      this.followPath(char);
-    },
-  },
-  task:{
-    start: function(){},
-    update: function(char){
-      let task = this.taskManager.getTask(char.task);
-      this.move(char, centerBlock(blockToPoint(task.block)), this.time.deltaTime*config.character.speed);
-      if(atBlock(char, task.block)){
-        task.progress += this.time.deltaTime;
-        if(task.progress>=1){
-          this.gridManager.addNode(task.block.x, task.block.y, task.grid);
-          this.taskManager.finishTask(char.task);
-          this.changeState(char, States.IDLE);
-        }
-      }
-    },
-    end: function(char){
-      delete char.task;
-    },
-  }
-};
+
+
 
 export default class Character{
   constructor(state){
