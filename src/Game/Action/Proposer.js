e0c57d0c95e3725baa941b274386de3be38a84e3
digijin <james@digijin.com>
2016-09-04 01:49:49 +1000

    proposal spec

---
+++ src/Game/Action/Proposer.js
@@ -0,0 +1,22 @@
+/*
+ACTION PROPOSER
+this comes up with a proposal
+based on ui and selection
+
+output used for:
+ - rendering preview
+ - checking resources
+ - implementing in dispatcher
+
+inputs:
+ - selection
+ - ui state
+
+output:
+ - delta state
+ - resource cost
+
+processing:
+ - remove duplicates
+
+*/
---
+++ src/Game/Action/Proposer.spec.js
@@ -0,0 +1,3 @@
+describe('Game/Action/Proposer', () => {
+  
+})
