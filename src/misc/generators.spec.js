f7472494f4d57d63da464a263ea810da30bf3e07
digijin <james@digijin.com>
2016-09-14 18:31:39 +1000

    generator variables

--- src/misc/generators.spec.js
+++ src/misc/generators.spec.js
@@ -10,24 +10,30 @@ describe('generators', () => {
   });
 
   it('should nest generators', () => {
-    function* child(){
+    function* child(name){
       yield 'a';
       yield 'b';
       yield 'c';
+      yield name;
     }
-    function* parent(){
+    function* parent(inp){
       yield 1;
       yield 2;
-      yield *child();
+      yield *child('bob');
+      inp.test = 'test';
       yield 3;
     }
-    let gen = parent();
+    let input = {};
+    let gen = parent(input);
 
     expect(gen.next().value).toBe(1);
     expect(gen.next().value).toBe(2);
     expect(gen.next().value).toBe("a");
     expect(gen.next().value).toBe("b");
     expect(gen.next().value).toBe("c");
+    expect(gen.next().value).toBe("bob");
+    expect(input.test).not.toBeDefined();
     expect(gen.next().value).toBe(3);
+    expect(input.test).toBe('test');
   });
 });
