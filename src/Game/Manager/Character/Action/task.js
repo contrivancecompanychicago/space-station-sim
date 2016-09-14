20747b4242b2e117ed4a8c5640acf9152d0ff454
digijin <james@digijin.com>
2016-09-14 20:56:02 +1000

    task action

---
+++ src/Game/Manager/Character/Action/task.js
@@ -0,0 +1,16 @@
+import engine from 'Game/engine';
+import moveToBlock from './moveToBlock';
+import time from 'Game/time';
+
+export default function* task(char){
+  let taskManager = engine.getComponent('taskManager');
+  let task = taskManager.getTask(char.task);
+  yield *moveToBlock(char, task.block);
+  while(task.progress<1){
+    task.progress += time.deltaTime;
+    yield;
+  }
+  let gridManager = engine.getComponent('gridManager');
+  gridManager.addNode(task.block.x, task.block.y, task.grid);
+  taskManager.finishTask(char.task);
+}
