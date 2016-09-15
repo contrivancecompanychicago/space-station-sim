a5b4c0231441460a3ba4d6480db48380e1ca9fb3
digijin <james@digijin.com>
2016-09-15 21:18:05 +1000

    cleanup

--- src/Game/Manager/Character.js
+++ src/Game/Manager/Character.js
@@ -4,30 +4,9 @@ import {keys, defaults} from 'lodash';
 
 import config from 'Game/config';
 
-import {blockToPoint, pointToBlock, blockToCenter, pointAtBlock} from 'Util';
-
 import State from './Character/State';
 
-// import wander from './Character/Action/wander';
 import actions from 'Game/Manager/Character/Action';
-
-function centerBlock(block){
-  return {
-    x: block.x + 16,
-    y: block.y + 16
-  };
-}
-function atBlock (char, target){
-  let myBlock = pointToBlock(char);
-  return (myBlock.x === target.x && myBlock.y === target.y);
-}
-
-// const States = {
-//   IDLE:'idle',
-//   RANDOM: 'random',
-//   TASK: 'task'
-// };
-import States from './Character/States';
 
 
 
@@ -35,18 +14,10 @@ export default class Character{
   constructor(state){
     this.type = 'characterManager';
     this.state = state;
-    this.bindStates();
   }
-  bindStates() {
-    keys(State).forEach((k) => {
-      keys(State[k]).forEach((s) => {
-        State[k][s] = State[k][s].bind(this);
-      });
-    });
-  }
+
 
   addChar(char){
-
     if(!char.id)
       char.id = uniqid();
     defaults(char, {
@@ -55,20 +26,11 @@ export default class Character{
         y: 0
       },
       path: [],
-      state: States.IDLE
     });
     this.state[char.id] = char;
   }
 
-  changeState(char, newState) {
-    // console.log(char, 'to', newState);
-    let current = State[char.state];
-    let next = State[newState];
-    // console.log(char.state, current);
-    if(current.end) current.end(char);
-    char.state = newState;
-    if(next.start) next.start(char);
-  }
+
 
   update(time){
     this.time = time;
@@ -99,31 +61,7 @@ export default class Character{
     char.action = actions.wander(char);
   }
 
-  followPath(char){
-    if(char.targetBlock){
-      let point = char.targetBlock.center;
-      if(!point)debugger;
-      this.move(char, point, config.character.speed * this.time.deltaTime);
-      if(atBlock(char.position, char.targetBlock)){
-        delete char.targetBlock;
-      }
-    }else if(char.path.length>0){
-      char.targetBlock = char.path.shift();
-    }else{
-      this.changeState(char, States.IDLE);
-    }
-  }
-  makePath(char, target){
-    let currentBlock = char.position.block;
-    char.path = this.gridManager.getPath(currentBlock, target);
-  }
-  move(char, target, amount){
-    // if(!target)debugger;
-    // if(!char)debugger;
-    const dir = Math.atan2(target.y - char.position.y, target.x-char.position.x);
-    char.position.x += Math.cos(dir)*amount;
-    char.position.y += Math.sin(dir)*amount;
-  }
+
 
   getChar(id){
     return this.state[id];
