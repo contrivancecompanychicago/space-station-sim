210ede751154f2965699bdc18cd1422859b66b31
digi <james@digijin.com>
2017-01-18 00:05:18 +1100

    addnode bug

--- src/Game/Manager/Character/Action/task.js
+++ src/Game/Manager/Character/Action/task.js
@@ -8,6 +8,8 @@ import type Character from 'Game/Type/Character';
 
 import type TaskManager from 'Game/Manager/Task';
 
+import Grid from 'Game/Type/Grid'
+
 export default function* task(char:Character):Generator<*,*,*>{
   let taskManager:TaskManager = (engine.getComponent('taskManager'):any);
   let task = taskManager.getTask(char.task);
@@ -19,6 +21,6 @@ export default function* task(char:Character):Generator<*,*,*>{
   let gridManager = engine.getComponent('gridManager');
   //resolve task
 
-  gridManager.addNode(task.block.x, task.block.y, task.grid);
+  gridManager.addNode(task.block.x, task.block.y, new Grid({type: task.grid}));
   taskManager.finishTask(char.task);
 }
