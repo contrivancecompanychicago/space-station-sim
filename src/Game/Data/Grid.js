64f3c1e8cfeaeae9ea689e2633ccc8dde55f1250
digijin <james@digijin.com>
2017-01-18 20:25:04 +1100

    grid types

--- src/Game/Data/Grid.js
+++ src/Game/Data/Grid.js
@@ -3,6 +3,8 @@ import keys from 'lodash.keys';
 const Gridz = {
   'BASIC': {label: 'Basic', weight:1},
   'FLOOR': {label: 'Floor', weight:1, image:require('./Grid/plain.png')},
+  'TILES1': {label: 'Tiles', weight:1, image:require('./Grid/bw_tiles.png')},
+  'TILES2': {label: 'Tiles', weight:1, image:require('./Grid/browntiles.png')},
   'SHOP': {label: 'Shop', weight:2, image:require('./Grid/plain.png'), tint:'red'},
   'BAR': {label: 'Shop', weight:1, image:require('./Grid/plain.png'), tint:'green'},
   'WALL': {label: 'Wall', weight:0, image:require('./Grid/wall.png')},
