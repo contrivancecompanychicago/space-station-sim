caff11b359b0487fcff0a1e1d148fd8ac63b52a5
digijin <james@digijin.com>
2017-01-19 01:26:16 +1100

    idling

--- src/Game/Manager/Character/Action/cook.js
+++ src/Game/Manager/Character/Action/cook.js
@@ -1,19 +1,19 @@
 
 import engine from 'Game/engine';
 
-import pathToBlock from '././pathToBlock';
-import pathToObject from '././pathToObject';
+import pathToBlock from './pathToBlock';
+import pathToObject from './pathToObject';
+import idle from './idle';
 
 import {Obj} from 'Game/Data/Object';
 
 export default function* cook(char){
   let gridManager = engine.getComponent('gridManager');
   let objectManager = engine.getComponent('objectManager');
-  // let objs = objectManager.getObjectsOfType(Obj.FRIDGE);
-  // for(let i = 0; i< objs.length; i++){
-  //     yield *pathToBlock(char, objs[i].block);
-  // }
   yield *pathToObject(char, Obj.FRIDGE);
-  
+  yield *idle(char, 1);
+  yield *pathToObject(char, Obj.OVEN);
+  yield *idle(char, 1);
+
 
 }
--- src/Game/Manager/Character/Action/pathToObject.js
+++ src/Game/Manager/Character/Action/pathToObject.js
@@ -6,8 +6,9 @@ export default function* pathToObject(char, obj){
   let gridManager = engine.getComponent('gridManager');
   let objectManager = engine.getComponent('objectManager');
   let objs = objectManager.getObjectsOfType(obj);
-  for(let i = 0; i< objs.length; i++){
-      yield *pathToBlock(char, objs[i].block);
+  if(objs.length > 0){
+    let i = Math.floor(Math.random()*objs.length);
+    yield *pathToBlock(char, objs[i].block);
   }
 
 }
