d0fabc9cce567ced970f446b7a587babda38ba2e
digi <james@digijin.com>
2017-01-22 00:29:11 +1100

    make people wait for things

--- src/Game/Manager/Character/Action/cook.js
+++ src/Game/Manager/Character/Action/cook.js
@@ -6,13 +6,14 @@ import pathToObjectWithAbility from './pathToObjectWithAbility';
 import idle from './idle';
 import placeItemOnBlock from './placeItemOnBlock'
 import placeItemOnEmptyTable from './placeItemOnEmptyTable'
-
+import findObject from './findObject'
 // import {Obj} from 'Game/Data/Object';
 import Ability from 'Game/Data/Object/Ability'
 
 import type Character from 'Game/Type/Character'
 import Item from 'Game/Type/Item'
 import type ItemManager from 'Game/Manager/Item';
+import type Obj from 'Game/Type/Object'
 // import {ItemType} from 'Game/Data/Item'
 
 // import
@@ -26,18 +27,33 @@ export default function* cook(char:Character):Generator<*,*,*>{
     let item = new Item({position: obj.block.center, type:'TEST'})
     itemManager.addItem(item);
     char.item = item;
+  }else{
+    return;
   }
   yield *idle(char, 1);
-  obj = yield *pathToObjectWithAbility(char, Ability.PREP_TABLE);
-  if(obj){
-    yield *placeItemOnBlock(char, obj.block)
-  }
+
+  //FIND PREP TABLE
+  obj = yield *findObject((o:Obj) => {
+    if(o.character) return false;
+    if(o.getData().abilities.indexOf(Ability.PREP_TABLE) > -1) return true
+  })
+  // obj = yield *pathToObjectWithAbility(char, Ability.PREP_TABLE);
+  obj.character = char;
+  yield *pathToBlock(char, obj.block);
+  yield *placeItemOnBlock(char, obj.block)
   yield *idle(char, 1);
-  obj = yield *pathToObjectWithAbility(char, Ability.OVEN);
-  if(obj){
-    yield *placeItemOnBlock(char, obj.block)
-  }
+  obj.character = null;
+
+  //FIND OVEN
+  obj = yield *findObject((o:Obj) => {
+    if(o.character) return false;
+    if(o.getData().abilities.indexOf(Ability.OVEN) > -1) return true
+  })
+  obj.character = char;
+  yield *pathToBlock(char, obj.block);
+  yield *placeItemOnBlock(char, obj.block)
   yield *idle(char, 1);
+  obj.character = null;
   // obj = yield *pathToObjectWithAbility(char, Ability.SERVE_TABLE);
   // if(obj){
   //   yield *placeItemOnBlock(char, obj.block)
---
+++ src/Game/Manager/Character/Action/findObject.js
@@ -0,0 +1,18 @@
+//@flow
+import engine from 'Game/engine'
+
+import type Obj from 'Game/Type/Object'
+import type ObjectManager from 'Game/Manager/Object'
+
+export default function* findObject(filter:Function):Generator<*,Obj,*>{
+  let objectManager:ObjectManager = engine.getComponent('objectManager');
+  let obj:Obj
+
+  while(!obj){
+    let objs = objectManager.getObjects().filter(filter);
+    if(objs.length>0) obj = objs[0];
+    yield;
+  }
+  return obj
+
+}
