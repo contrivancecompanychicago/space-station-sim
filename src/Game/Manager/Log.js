f41885879a21f63987c9ad5c6ad91c580baf5604
digijin <james@digijin.com>
2017-01-27 17:41:09 +1100

    tweak

--- src/Game/Manager/Log.js
+++ src/Game/Manager/Log.js
@@ -1,17 +1,19 @@
 // @flow
 import {keys, defaults} from 'lodash';
 
-export type LogType = 'EVENT'
+export type LogType = 'EVENT'|'ORDER'|'SERVE'
+
+export type LogEntry = {type:LogType, message:string}
 
 export default class LogManager{
   type: string;
-  log: Array<string>;
+  log: Array<LogEntry>;
   constructor(){
     this.type = 'logManager';
     this.log = []
   }
-  log(message:{type:LogType, message:string}){
-    this.log.push(message.message);
+  log(message:LogEntry){
+    this.log.push(message);
     if(this.log.length>20){
       this.log.shift();
     }
