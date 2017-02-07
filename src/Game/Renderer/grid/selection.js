0d1856f86e236b936c04dcad45a1ec785dd96d03
digijin <james@digijin.com>
2017-02-07 20:07:20 +1100

    finished moving everything to block point

--- src/Game/Renderer/grid/selection.js
+++ src/Game/Renderer/grid/selection.js
@@ -1,12 +1,14 @@
 // @flow
 import {assign} from 'lodash';
-import {worldToScreen, blockToPoint} from 'Util';
+// import {worldToScreen, blockToPoint} from 'Util';
 import config from 'Game/config';
 
 import type {State} from 'Game/state'
 import type Layer from 'Game/Renderer/Layer'
 
 import type {Selection} from 'Game/Type/Selection'
+import Block from 'Game/Block'
+import Point from 'Game/Point'
 
 export default function renderSelection(state:State, layer:Layer){
 
@@ -17,32 +19,16 @@ export default function renderSelection(state:State, layer:Layer){
     assign(layer.context, config.view.selection);
     // FLOWHACK selection is defined
     let r = state.View.selection.rect.blockRect();
-    let tl = worldToScreen(blockToPoint({
-      x: r.l,
-      y: r.t
-    }), state);
-    let br = worldToScreen(blockToPoint({
-      x: (r.r + 1),
-      y: (r.b + 1)
-    }), state);
+    let tl = new Block({x: r.l, y: r.t}).point.screen
+    let br = new Block({x: r.r+1, y: r.b+1}).point.screen
     layer.context.strokeRect(tl.x, tl.y, br.x - tl.x, br.y - tl.y);
-
     layer.context.strokeStyle="green";
     layer.context.shadowColor="green";
     // FLOWHACK selection is defined
     r = state.View.selection.rect;
-    tl = worldToScreen(({
-      x: r.l,
-      y: r.t
-    }), state);
-    br = worldToScreen(({
-      x: (r.r + 1),
-      y: (r.b + 1)
-    }), state);
+    tl = new Point(r.l, r.t).screen
+    br = new Point(r.r+1, r.b+1).screen
     layer.context.strokeRect(tl.x, tl.y, br.x - tl.x, br.y - tl.y);
-
-
-
   }
-    layer.context.shadowBlur=0;
+  layer.context.shadowBlur=0;
 }
