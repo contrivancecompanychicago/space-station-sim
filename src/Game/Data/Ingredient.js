b9bb1eef19b8a22cdcb012c07f323ccf111a2a0e
digijin <james@digijin.com>
2017-02-10 17:17:49 +1100

    unstructured ingredients

---
+++ src/Game/Data/Ingredient.js
@@ -0,0 +1,99 @@
+
+[
+    mozzarella
+    sauce
+    BBQ
+    tomato
+    oregano
+    rosemary
+    basil
+
+    garlic
+    chili
+    salami
+    anchovy
+    capsicum
+    olive
+    ham
+    mushroom
+    sausage
+    potato
+    parmesan
+    olive oil
+    bacon
+    cabanossi
+    seafood
+    prawn
+    chicken
+    spinach
+    rocket
+    pineapple
+    onion
+
+
+
+
+]
+
+recipes:{
+    garlic: {
+        garlic
+    }
+    garliccheese{
+        garlic
+        mozzarella
+    }
+    margherita:{
+        sauce
+        mozzarella
+        oregano
+    }
+    americana: {
+        tomato sauce
+        salami
+        mozzarella
+        oregano
+    }
+    hawaiian: {
+        sauce
+        mozzarella
+        ham
+        pineapple
+    }
+    capricciosa: {
+        sauce
+        ham
+        mushroom
+        sausage
+        mozzarella
+        oregano
+        chili
+    }
+    contadina: {
+        potato
+        sausage
+        mozzarella
+        rosemary
+        oregano
+        garlic
+    }
+    calabrese: {
+        sauce
+        onion
+        salami
+        capsicum
+        anchovy
+        olive
+        mozzarella
+        oregano
+    }
+    funghi: {
+        sauce
+        mushroom
+        mozzarella
+        oregano
+        parmesan
+        garlic
+    }
+    gamberi
+}
