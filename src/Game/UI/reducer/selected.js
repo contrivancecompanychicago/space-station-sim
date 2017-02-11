6e9d9795b366d7f445c00437167794f1b730f552
digijin <james@digijin.com>
2017-02-11 23:59:25 +1100

    only one selected at any time (leaving code to reimplement more complex selection)

--- src/Game/UI/reducer/selected.js
+++ src/Game/UI/reducer/selected.js
@@ -5,8 +5,9 @@
 export default function selected(state:Array<Object> = [], action:Object):Array<Object>{
   switch(action.type){
     case 'SET_SELECTED':
-        if(state.indexOf(action.selected)>-1) return state;
-        state = [...state, action.selected];
+        // if(state.indexOf(action.selected)>-1) return state;
+        // state = [...state, action.selected];
+        state = [action.selected];
         
     break;
     case 'REMOVE_SELECTED':
