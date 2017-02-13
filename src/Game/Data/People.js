8c3b9a5452e1c4faa8d4d63ac1818fec7a9da49a
digi <james@digijin.com>
2017-02-13 23:51:27 +1100

    people crap

--- src/Game/Data/People.js
+++ src/Game/Data/People.js
@@ -1,9 +1,15 @@
 //straight thuggin
 PAPA GIOVANNI
  - lol he ded
-PROFESSOR FUCKING McNIGGER
+PROFESSOR DJ FUCKING McNIGGER
+ - eat a dick
  - eat a bag of dicks
+ - i fucked ur mum
+ - wut now bithc
+ - REKT
 DODGY PETE
 LIUETENANT SENIOR CONSTABLE DOOGIE HOWSER MD
+ - tally ho pip pip and whatnot
+ - whats this ere then laddie
 GOD
  - Im not helping unless you get at least 100 facebook likes
