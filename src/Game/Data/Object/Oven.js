0fbd3b09593ffee70e1b5f3dd838feb150c850f7
digijin <james@digijin.com>
2017-03-04 18:51:38 +1100

    ovennn

--- src/Game/Data/Object/Oven.js
+++ src/Game/Data/Object/Oven.js
@@ -9,16 +9,6 @@ const b = 'BLOCK';
 import Weight from 'Game/Data/Grid/Weight'
 
 let img = require('./oven.png');
-// //upsize image
-// var src = document.createElement("canvas");
-// src.width = img.width;
-// src.height = img.height;
-// src.getContext("2d").drawImage(img, 0, 0);
-// let pixel = src.getContext('2d').getImageData(1, 1, 1, 1).data
-
-// var image = new Image();
-// image.src = src.toDataURL("image/png");
-// debugger;
 
 const obj:ObjectDataType = {
   id: 'OVEN',
@@ -30,7 +20,7 @@ const obj:ObjectDataType = {
     {type: a, weight: Weight.ACCESS, x:0, y:0},
     {type: b, weight: Weight.BLOCK, x:0, y:1},
     {type: b, weight: Weight.BLOCK, x:0, y:2},
-    {type: b, weight: Weight.BLOCK, x:0, y:3},
+    // {type: b, weight: Weight.BLOCK, x:0, y:3},
   ],
   requirements: {},
   abilities: [Ability.OVEN]
