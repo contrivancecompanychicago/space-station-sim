22cd8b74266b804efaccaeb7ec83e993734e89ae
digijin <james@digijin.com>
2017-03-05 05:53:37 +1100

    customer always putting shit on table

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['coverage', 'progress', 'kjhtml'], // 'nyan',
+    // reporters: ['coverage', 'progress', 'kjhtml'], // 'nyan',
+    reporters: ['spec'],
 
     coverageReporter: {
       reporters:[
--- src/Game/Manager/Character/Action/customer.js
+++ src/Game/Manager/Character/Action/customer.js
@@ -14,7 +14,7 @@ import type Obj from 'Game/Type/Object'
 import Order from 'Game/Type/Order'
 export default function* customer(char: Character): Generator<*,*,*>{
 
-  let itemManager = state.item
+	let itemManager = state.item
   let gridManager = state.grid
   let objectManager = state.object
   let orderManager = state.order
@@ -22,77 +22,81 @@ export default function* customer(char: Character): Generator<*,*,*>{
   
   char.setStatus('Sitting down')
   let chair = yield * actions.pathToObjectWithAbility(char, Ability.CHAIR);
-  if(chair) {
-    chair.setCharacter(char)
-    yield * actions.moveToBlockCenter(char, chair.block)
+	if(chair) {
+		chair.setCharacter(char)
+		yield * actions.moveToBlockCenter(char, chair.block)
 
-    //PLACE ORDER!
-    let orders: Array<Order> = []
+		//PLACE ORDER!
+		let orders: Array<Order> = []
 
-    let pizza = new Order({ customer: char, type: 'PIZZA' })
-    orders.push(pizza)
-    orderManager.addOrder(pizza);
-    let coffee = new Order({ customer: char, type: 'COFFEE' })
-    orders.push(coffee)
-    orderManager.addOrder(coffee);
-    let numFulfilled = 0;
-    while (numFulfilled !== orders.length) {
-      char.setStatus('waiting for order (' + numFulfilled + '/' + orders.length + ')')
-      numFulfilled = 0;
-      orders.forEach((o) => {
-        if (o.status === 'FULFILLED') numFulfilled++;
-      })
-      yield; //wait til I get my shit.
-    } 0
-    chair.removeCharacter();
+		let pizza = new Order({ customer: char, type: 'PIZZA' })
+		orders.push(pizza)
+		orderManager.addOrder(pizza);
+		let coffee = new Order({ customer: char, type: 'COFFEE' })
+		orders.push(coffee)
+		orderManager.addOrder(coffee);
 
-    //check for table;
-    let checks = [
-      {x:1, y:0},
-      {x:-1, y:0},
-      {x:0, y:-1},
-      {x:0, y:1},
-    ]
-    let table
-    checks.forEach(c => {
-      let check = char.position.block
-      check.x += c.x
-      check.y += c.y
-      if(!table){
-        table = objectManager.getObjectAtBlock(check);
-        if (table) {
-          if(!table.hasAbility('DINE_TABLE')){
-            table = null;
-          }
-        }
-      }
-    })
-    if(table){
-      yield * actions.placeItemOnBlock(char, table.block)
-    }
-    char.setStatus('Eating')
-    yield * actions.idle(char, 5);
-    state.player.addMoney(20)
-    while (char.item.length > 0) {
-      let item = char.item[0];
-      itemManager.removeItem(item);
-      char.removeItem(item);
-    }
-    char.setStatus('Leaving')
-    yield * actions.pathToObjectWithAbility(char, Ability.SPAWN)
 
-  }else{
-    char.setStatus('Nowhere to sit')
-    yield *actions.pathToObjectWithAbility(char, Ability.SPAWN)
-  }
+		//check for table;
+		let checks = [
+			{ x: 1, y: 0 },
+			{ x: -1, y: 0 },
+			{ x: 0, y: -1 },
+			{ x: 0, y: 1 },
+		]
+		let table
+		checks.forEach(c => {
+			let check = char.position.block
+			check.x += c.x
+			check.y += c.y
+			if (!table) {
+				table = objectManager.getObjectAtBlock(check);
+				if (table) {
+					if (!table.hasAbility('DINE_TABLE')) {
+						table = null;
+					}
+				}
+			}
+		})
+
+
+		let numFulfilled = 0;
+		while (numFulfilled !== orders.length) {
+			char.setStatus('waiting for order (' + numFulfilled + '/' + orders.length + ')')
+			numFulfilled = 0;
+			orders.forEach((o) => {
+				if (o.status === 'FULFILLED') numFulfilled++;
+			})
+			yield; //wait til I get my shit.
+			if (table) {
+				yield * actions.placeItemOnBlock(char, table.block)
+			}
+
+		}
+		chair.removeCharacter();
+		char.setStatus('Eating')
+		yield * actions.idle(char, 5);
+		state.player.addMoney(20)
+		while (char.item.length > 0) {
+			let item = char.item[0];
+			itemManager.removeItem(item);
+			char.removeItem(item);
+		}
+		char.setStatus('Leaving')
+		yield * actions.pathToObjectWithAbility(char, Ability.SPAWN)
+
+	}else{
+		char.setStatus('Nowhere to sit')
+		yield *actions.pathToObjectWithAbility(char, Ability.SPAWN)
+	}
   // yield *actions.wander(char);
 
   //WIPE CLEAN - hacky
 
 
-  char.setStatus('Gone')
+	char.setStatus('Gone')
 
-  charManager.removeCharacter(char);
+	charManager.removeCharacter(char);
 
 
 
