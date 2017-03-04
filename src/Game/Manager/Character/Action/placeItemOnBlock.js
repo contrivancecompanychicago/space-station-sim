2c9b052bc149df2f4fdb108dad2532d96877085b
digijin <james@digijin.com>
2017-03-05 06:02:52 +1100

    placing coffee next to pizza

--- src/Game/Manager/Character/Action/placeItemOnBlock.js
+++ src/Game/Manager/Character/Action/placeItemOnBlock.js
@@ -7,27 +7,31 @@ import type Block from 'Game/Block'
 
 export default function* placeItemOnBlock(char:Character, block:Block):Generator<*,*,*>{
 
-  let target = block.center;
-  for(let i = 0; i< char.item.length; i++){
+	let items = char.getItems()
+	for(let i = 0; i< items.length; i++){
 
-  // }
-  // if(char.item.length>0){
-    let item = char.item[i]
-    let dist = distance(target, item.position);
-    let amount = 0;
-    while(dist>amount){
-      amount = time.deltaTime * config.character.speed;
-      let dir = Math.atan2(target.y - char.position.y, target.x - char.position.x);
-      item.position.x -= (item.position.x - target.x) /6
-      item.position.y -= (item.position.y - target.y) /6
-      dist = distance(target, item.position);
+		let item = items[i]
+		let target = block.center;
+		if(item.type == 'COFFEE'){
+			target.x += 10;
+		}
 
-      yield;
-    }
 
-  }
+		let dist = distance(target, item.position);
+		let amount = 0;
+		while(dist>amount){
+			amount = time.deltaTime * config.character.speed;
+			let dir = Math.atan2(target.y - char.position.y, target.x - char.position.x);
+			item.position.x -= (item.position.x - target.x) /6
+			item.position.y -= (item.position.y - target.y) /6
+			dist = distance(target, item.position);
+
+			yield;
+		}
+
+	}
 }
 
 function distance(a,b){
-  return Math.sqrt(Math.pow(a.y - b.y, 2)+Math.pow(a.x - b.x, 2))
+	return Math.sqrt(Math.pow(a.y - b.y, 2)+Math.pow(a.x - b.x, 2))
 }
