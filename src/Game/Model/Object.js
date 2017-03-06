935aeac9f4a2e2fc68603e32335934ae92859e38
digijin <james@digijin.com>
2017-03-06 21:01:29 +1100

    improving grid object cache

--- src/Game/Model/Object.js
+++ src/Game/Model/Object.js
@@ -14,95 +14,104 @@ import makeKey from 'Util/makeKey';
 
 
 export type ObjectState = {
-  [id: string]: Obj
+	[id: string]: Obj
 }
 
 export default class ObjectModel {
-  state: ObjectState;
-  constructor(s: ObjectState = {}) {
-    this.state = s;
-  }
-  addObject(obj: Obj) {
-    //todo: check overlaps
-    //todo: valid/verify/factory
-    // obj = Factory.create(obj);
-    this.state[obj.getKey()] = obj;
+	state: ObjectState;
+	constructor(s: ObjectState = {}) {
+		this.state = s;
+	}
+	addObject(obj: Obj) {
+		//todo: check overlaps
+		//todo: valid/verify/factory
+		// obj = Factory.create(obj);
+		this.state[obj.getKey()] = obj;
 
-  }
-  deleteObject(obj: Obj) {
-    delete this.state[obj.getKey()];
-  }
-  getObject(key: string) {
-    return this.state[key]
-  }
-  getObjectWithKey(key: string) {
-    return this.state[key]
-  }
-  getObjectAtBlock(block: Block):?Obj{
-    if(this.state[block.key])
-      return this.state[block.key];
+	}
+	deleteObject(obj: Obj) {
+		delete this.state[obj.getKey()];
+	}
+	getObject(key: string) {
+		return this.state[key]
+	}
+	getObjectWithKey(key: string) {
+		return this.state[key]
+	}
+	getObjectAtBlock(block: Block):?Obj{
+		if(this.state[block.key])
+			return this.state[block.key];
 
-    //check overlapping
-    let grid = state.grid.getNode(block.x, block.y)
-    if(grid) {
-      if (grid.object) {
-        return this.state[grid.object];
-      }
-    }
+		//check overlapping
+		let grid = state.grid.getNode(block.x, block.y)
+		if(grid) {
+			if (grid.object) {
+				return this.state[grid.object];
+			}
+		}
 
-  }
-  getObjects(): Array<Obj> {
-    return values(this.state);
-  }
-  getObjectsOfType(type: string): Array<Obj> {
-    return values(this.state).filter((o) => { return o.type === type })
-  }
-  getObjectsWithAbility(ability: AbilityType): Array<Obj> {
-    return values(this.state).filter((o) => {
-      let type = ObjectData.get(o.type)
-      if (type.abilities.indexOf(ability) > -1) return true
-    })
-  }
+	}
+	getObjects(): Array<Obj> {
+		return values(this.state);
+	}
+	getObjectsOfType(type: string): Array<Obj> {
+		return values(this.state).filter((o) => { return o.type === type })
+	}
+	getObjectsWithAbility(ability: AbilityType): Array<Obj> {
+		return values(this.state).filter((o) => {
+			let type = ObjectData.get(o.type)
+			if (type.abilities.indexOf(ability) > -1) return true
+		})
+	}
 
-  getObjectsWithItemType(type: ItemType): Array<Obj> {
-    return values(this.state).filter((o) => {
-      if (o.item) {
-        if (o.item.type === type) return true
-      }
-    })
-  }
+	getObjectsWithItemType(type: ItemType): Array<Obj> {
+		return values(this.state).filter((o) => {
+			if (o.item) {
+				if (o.item.type === type) return true
+			}
+		})
+	}
 
-  mergeState(objects: ObjectState) {
-    extend(state.object.state, objects)
-    keys(objects).forEach((key) => {
-      let obj = objects[key];
+	mergeState(objects: ObjectState) {
+		extend(state.object.state, objects)
+		this.cacheObjects(objects);
+	}
+	cacheObjects(objects: ObjectState){
+		keys(objects).forEach((key) => {
+			let obj = objects[key];
+			this.cacheObject(obj)
+		})
+	}
+	cacheObject(obj:Obj){
+		let type = ObjectData.get(obj.type)
 
-      let type = ObjectData.get(obj.type)
+		// let coord = parseKey(key)
+		let coord = obj.block;
+		let key = makeKey(coord.x, coord.y)
+		for (let x = 0; x < type.width; x++) {
+			for (let y = 0; y < type.height; y++) {
+				let gridkey = makeKey(coord.x + x, coord.y + y)
+				if (state.grid.state[gridkey])
+					state.grid.state[gridkey].object = key;
+			}
+		}
 
-      let coord = parseKey(key)
-      for (let x = 0; x < type.width; x++) {
-        for (let y = 0; y < type.height; y++) {
-          let gridkey = makeKey(coord.x + x, coord.y + y)
-          if (state.grid.state[gridkey])
-            state.grid.state[gridkey].object = key;
-        }
-      }
-    })
-  }
-  save(): Object {
-    return this.state;
-  }
-  clear() {
-    this.state = {}
-  }
-  load(object: Object) {
-    Object.keys(object).forEach((key) => {
-      let obj = object[key];
-      obj.block = new Block(obj.block)
-      object[key] = new Obj(obj);
-    })
-    this.mergeState(object)
-  }
+	}
+	
+	save(): Object {
+		return this.state;
+	}
+	clear() {
+		this.state = {}
+	}
+	load(object: Object) {
+		Object.keys(object).forEach((key) => {
+			let obj = object[key];
+			obj.block = new Block(obj.block)
+			object[key] = new Obj(obj);
+		})
+		this.mergeState(object)
+	}
 
 
 }
--- src/Game/state.integration.spec.js
+++ src/Game/state.integration.spec.js
@@ -17,18 +17,29 @@ describe('Game/state integration', () => {
 		expect(state.grid).toBeDefined();
 	});
 
-	it('should have grid nodes find objects', () => {
-		let start = new Block({x: 0, y: 0}).center;
-		let end = new Block({x: 1, y: 3}).center;
-		state.grid.addNodes(new Rect(start, end), new Grid({type:'FLOOR'}))
-		expect(Object.keys(state.grid.state).length).toBe(8);
+	describe('grid object integration', () => {
 
-		let node = state.grid.getNode(0,0);
-		expect(node.type).toBe('FLOOR');
-		node = state.grid.getNode(1,3);
-		expect(node.type).toBe('FLOOR');
-		let obj = new Obj({block:new Block({x:0, y:0}), type:'FRIDGE'});
-		state.object.addObject(obj)
+		beforeEach(() => {
+			let start = new Block({x: 0, y: 0}).center;
+			let end = new Block({x: 1, y: 3}).center;
+			state.grid.addNodes(new Rect(start, end), new Grid({type:'FLOOR', rotation:0}))
+
+		})
+		it('sohuld set up tests proper-like', () => {
+			expect(Object.keys(state.grid.state).length).toBe(8);
+			let node = state.grid.getNode(0,0);
+			expect(node.type).toBe('FLOOR');
+			node = state.grid.getNode(1,3);
+			expect(node.type).toBe('FLOOR');
+		})
+
+		it('should lookup obj from node', () => {
+			let obj = new Obj({block:new Block({x:0, y:0}), type:'FRIDGE'});
+			state.object.addObject(obj)
+			let node = state.grid.getNode(0,0);
+			expect(node.getObject()).toBe(obj);
+
+		})
 	})
 
 })
