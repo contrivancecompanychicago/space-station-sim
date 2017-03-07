203b90dc7130915080e272099117d0bc7264e0d6
digijin <james@digijin.com>
2017-03-08 02:23:18 +1100

    unused characters

--- src/Game/Data/Character.js
+++ src/Game/Data/Character.js
@@ -17,8 +17,8 @@ const CharMap:DataMap<CharacterType, CharacterDataType> = new DataMap();
 
 const Chars:{[id:CharacterType]: CharacterDataType} = {
   'WORKER': {label: 'worker', tint:'yellow', image:require('./Character/char1.png')},
-  'COOK': {label: 'cook', tint:'white', image:require('./Character/char5.png')},
-  'WAITER': {label: 'waiter', tint: 'blue', image:require('./Character/char3.png')},
+  // 'COOK': {label: 'cook', tint:'white', image:require('./Character/char5.png')},
+  // 'WAITER': {label: 'waiter', tint: 'blue', image:require('./Character/char3.png')},
   'CUSTOMER': {label: 'customer', tint: 'green', image:require('./Character/char4.png')},
 };
 
