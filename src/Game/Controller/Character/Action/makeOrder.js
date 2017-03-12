7e5ffaff9051f8a355da34e2e6b9214bde3e667e
digi <james@digijin.com>
2017-03-13 00:29:43 +1100

    makeorder split out next step function

--- src/Game/Controller/Character/Action/makeOrder.js
+++ src/Game/Controller/Character/Action/makeOrder.js
@@ -8,39 +8,53 @@ import ItemData from 'Game/Data/Item'
 
 /** takes one step in order manufacture */
 export default function* makeOrder(char:Character, order:Order):Generator<*,*,*>{
+
+    let item:?Item = order.getItem();
+    let making = nextStep(order);
+    if(!making) return; //nothing to do;
+
+    let data = ItemData.get(making);
+    if(data.requires.itemType){
+        //get our required item types
+        if(!item)throw new Error('item doesnt exist nextStep failing')
+        
+        if(!char.hasItem(item)){
+            // TODO go pick up item
+        }
+    }
+
+    // debugger;
+
+}
+
+export function nextStep(order:Order):string|boolean{
     let item:?Item = order.getItem();
     let wanted = order.type
-    debugger;
+    // debugger;
     // let wanted = ItemData.get(order.type);
     //find what step we are at
     if(item && item.type == wanted){
-        //at the right step
-        return wanted
+        return false //its already made nothing to do
     }
     let data = ItemData.get(wanted)
     let reqd  = data.requires.itemType
-    
     if(item && item.type == reqd){
         //at the right step
-        yield wanted
+        return wanted
     }
     wanted = reqd;
+
     data = ItemData.get(wanted)
     reqd  = data.requires.itemType
-
     if(item && item.type == reqd){
         //at the right step
-        yield wanted
+        return wanted
     }
     wanted = reqd;
 
     data = ItemData.get(wanted)
     reqd = data.requires.itemType
     if(!reqd){
-        // console.log('base');
-        yield wanted;
+        return wanted;
     }
-
-
-    return 'end';
 }
--- src/Game/Controller/Character/Action/makeOrder.spec.js
+++ src/Game/Controller/Character/Action/makeOrder.spec.js
@@ -1,6 +1,6 @@
 // @flow
 
-import makeOrder from './makeOrder'
+import makeOrder, {nextStep} from './makeOrder'
 
 import state from 'Game/state'
 import Character from 'Game/Type/Character'
@@ -22,20 +22,34 @@ describe('actions/makeOrder', () => {
     it('should be a function', () => {
         expect(typeof makeOrder).toBe('function');
     })
+    describe('nextStep', () => {
 
-    it('should return base if no item', () => {
-        expect(makeOrder(char, order).next().value).toBe('BASE')
+        it('should return base if no item', () => {
+            expect(nextStep(order)).toBe('BASE')
+        })
+        it('should return PIZZAUNCOOKED if there is a base', () => {
+            item = new Item({type:'BASE'});
+            state.item.addItem(item);
+            order.setItem(item);
+            expect(nextStep(order)).toBe('PIZZAUNCOOKED')
+        })
+        it('should return PIZZA if there is a uncooked', () => {
+            item = new Item({type:'PIZZAUNCOOKED'});
+            state.item.addItem(item);
+            order.setItem(item);
+            expect(nextStep(order)).toBe('PIZZA')
+        })
+        it('should return ??? if the pizza is already made', () => {
+            
+            item = new Item({type:'PIZZA'});
+            state.item.addItem(item);
+            order.setItem(item);
+            expect(nextStep(order)).toBe(false)
+        })
+
     })
-    it('should return PIZZAUNCOOKED if there is a base', () => {
-        item = new Item({type:'BASE'});
-        state.item.addItem(item);
-        order.setItem(item);
-        expect(makeOrder(char, order).next().value).toBe('PIZZAUNCOOKED')
-    })
-    it('should return PIZZA if there is a uncooked', () => {
-        item = new Item({type:'PIZZAUNCOOKED'});
-        state.item.addItem(item);
-        order.setItem(item);
-        expect(makeOrder(char, order).next().value).toBe('PIZZA')
+    it('should do something', () => {
+        let gen = makeOrder(char, order);
+        gen.next();
     })
 })
