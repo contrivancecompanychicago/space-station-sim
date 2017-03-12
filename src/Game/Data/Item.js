fe3d9df40a4b832bc38ffc4f1ae6eb8fe5ba23fb
digi <james@digijin.com>
2017-03-12 23:19:44 +1100

    item metadata for construction

--- src/Game/Data/Item.js
+++ src/Game/Data/Item.js
@@ -4,22 +4,69 @@ Items are easily movable entities that only have a position
 */
 import DataMap from 'Game/Data/Map'
 import {keys} from 'lodash';
+import type {TaskType} from 'Game/Data/Task'
+import type {AbilityType} from 'Game/Data/Object/Ability'
 
 export type ItemType = 'TEST'|'BASE'|'PIZZAUNCOOKED'|'PIZZA'|'PIZZADONE'|'COFFEE'|'COFFEEDONE'
 
+export type ItemRequires = {
+  objectAbility?: AbilityType, //todo more specific
+  itemType?: ItemType,
+  characterTaskType?: TaskType
+}
 export type ItemDataType = {
   label:string,
-  image:any
+  image:any,
+  requires: ItemRequires,
 }
 
 const Items:{[id:ItemType]:ItemDataType} = {
-  'TEST': {label: 'test item', image: require('./Item/pizza.png')},
-  'BASE': {label: 'ingredients', image: require('./Item/pizzabase.png')},
-  'PIZZAUNCOOKED': {label: 'ingredients', image: require('./Item/pizzauncooked.png')},
-  'PIZZA': {label: 'ingredients', image: require('./Item/pizza.png')},
-  'PIZZADONE': {label: 'ingredients', image: require('./Item/pizza.png')},
-  'COFFEE': {label: 'ingredients', image: require('./Item/coffee.png')},
-  'COFFEEDONE': {label: 'ingredients', image: require('./Item/coffee_empty.png')},
+  'TEST': {
+    label: 'test item', 
+    requires:{},
+    image: require('./Item/pizza.png')
+  },
+  'BASE': {
+    label: 'ingredients', 
+    requires:{
+      objectAbility: 'FRIDGE',
+      characterTaskType: 'MAKE'
+    },
+    image: require('./Item/pizzabase.png')
+  },
+  'PIZZAUNCOOKED': {
+    label: 'ingredients', 
+    requires:{
+      itemType: 'BASE',
+      objectAbility: 'PREP_TABLE',
+      characterTaskType: 'MAKE'
+    },
+    image: require('./Item/pizzauncooked.png')
+  },
+  'PIZZA': {
+    label: 'ingredients', 
+    requires:{
+      itemType: 'PIZZAUNCOOKED',
+      objectAbility: 'OVEN',
+      characterTaskType: 'COOK'
+    },
+    image: require('./Item/pizza.png')
+  },
+  // 'PIZZADONE': {
+  //   label: 'ingredients', 
+  //   requires:{},
+  //   image: require('./Item/pizza.png')
+  // },
+  'COFFEE': {
+    label: 'ingredients', 
+    requires:{},
+    image: require('./Item/coffee.png')
+  },
+  'COFFEEDONE': {
+    label: 'ingredients', 
+    requires:{},
+    image: require('./Item/coffee_empty.png')
+  },
 };
 
 // export default Items;
