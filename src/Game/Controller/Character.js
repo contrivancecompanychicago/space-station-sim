378fdaf79845359ff7e2c379f77270ff298000ed
digijin <james@digijin.com>
2017-03-20 13:48:37 +1100

    time based spawn - removing random

--- src/Game/Controller/Character.js
+++ src/Game/Controller/Character.js
@@ -4,47 +4,53 @@ import state from 'Game/state'
 import {keys} from 'lodash';
 import Ability from 'Game/Data/Object/Ability'
 
+import time from 'Game/time'
+
 import Character from 'Game/Type/Character'
 import customer from 'Game/Controller/Character/Action/customer'
 import worker from 'Game/Controller/Character/Action/worker'
 
-export default class CharacterController{
+const spawnGap = 5;
 
+export default class CharacterController{
+	spawnTimer:number;
+	constructor(){
+		this.spawnTimer = spawnGap;
+	}
 	update(){
 		state.character.getChars().forEach( char => {
 			if (!char.action) {
 				this.newAction(char);
 			}else{
-				// FLOWHACK action is set in previous if
 				if (char.action.next().done) { ///CALLS NEXT HERE
 					this.newAction(char);
 				}
-
 			}
 		});
 		this.spawnUpdate()
 		
 	}
 	spawnUpdate(){
-		//spawn;
-		let spawnPoints = state.object.getObjectsWithAbility(Ability.SPAWN)
-		spawnPoints.forEach((sp) => {
-			if (Math.random() < 0.0004) {
+		this.spawnTimer -= time.deltaTime;
+		if(this.spawnTimer<=0){
+			this.spawnTimer += spawnGap;
+			let spawnPoints = state.object.getObjectsWithAbility(Ability.SPAWN)
+			if(spawnPoints.length>0){
+				let sp = spawnPoints[Math.floor(Math.random()*spawnPoints.length)];
 				let char = new Character({ position: sp.block.center, type: 'CUSTOMER' })
-
 				state.character.addChar(char);
-
 			}
-		})
+		}
+		// //spawn;
+		// let spawnPoints = state.object.getObjectsWithAbility(Ability.SPAWN)
+		// spawnPoints.forEach((sp) => {
+		// 	if (Math.random() < 0.0004) {
+		// 		let char = new Character({ position: sp.block.center, type: 'CUSTOMER' })
+		// 		state.character.addChar(char);
+		// 	}
+		// })
 	}
 	newAction(char:Character){
-		let task = state.task.getUnassignedTask();
-		if (task) {
-			state.task.assignTask(task.id, char.id);
-			char.task = task.id;
-			char.action = actions.task(char);
-			return;
-		}
 
 		switch (char.type) {
 			case 'CUSTOMER':
