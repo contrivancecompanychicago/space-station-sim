39b90ed2ea9b24e0fc904f6dc32c723ae3103b15
digijin <james@digijin.com>
2017-03-31 23:13:37 +1100

    removing requirecontext cus it fucked tests up

--- src/Game/State/listSaves.js
+++ src/Game/State/listSaves.js
@@ -9,21 +9,19 @@ import config from 'Game/config'
 // import small from './saves/small'
 // import large from './saves/large'
 
-let ctx = require.context('./saves', false, /.js/);
+// let ctx = require.context('./saves', false, /.js/);
 // console.log('context is ', ctx);
 // debugger;
 
 export default function listSaves(){
 
-	let keys = ctx.keys();
-	keys.forEach((k) => {
-		localStorage[config.save.prefix+k] = JSON.stringify(ctx(k))
-	})
-	// localStorage['save_small'] = JSON.stringify(small)
-	// localStorage['save_large'] = JSON.stringify(large)
+	localStorage['save_livios'] = JSON.stringify(require('./saves/livios'))
 
-	// localStorage['save_demo1'] = JSON.stringify(demo1)
-	// localStorage['save_demo2'] = JSON.stringify(demo2)
+	// let keys = ctx.keys();
+	// keys.forEach((k) => {
+	// 	localStorage[config.save.prefix+k] = JSON.stringify(ctx(k))
+	// })
+
 	// localStorage['save_demo3'] = JSON.stringify(demo3)
 
 	return Object.keys(localStorage).filter((k) => {
