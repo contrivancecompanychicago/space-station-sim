b64a92743b72e3dd873f2763bdca7f8ba8af5d16
digijin <james@digijin.com>
2017-05-08 22:06:11 +1000

    dungeon maker

--- src/Game/Map.js
+++ src/Game/Map.js
@@ -8,10 +8,15 @@ import type State from 'Game/state'
 
 import Grid from 'Game/Type/Grid'
 export default function genMap(state:State){
-	for(let x = 0; x< 10; x++){
-		for(let y = 0; y< 10; y++){
-			let n = simplex.noise2D(1, 1);
-			state.grid.addNode(x,y, new Grid({type: 'FLOOR'}))
+	for(let x = 0; x< 100; x++){
+		for(let y = 0; y< 100; y++){
+			let n = simplex.noise2D(x/10, y/10);
+
+			let gridParams = {type: 'FLOOR'};
+			if(Math.abs(n) < 0.25) gridParams.type = 'WALLTEST'
+			state.grid.addNode(x,y, new Grid(gridParams))
+			
+
 		}
 	}
 }
