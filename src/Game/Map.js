420077674689eec07d4189b0e4e6bf9610de741b
digijin <james@digijin.com>
2017-05-09 00:50:18 +1000

    city block

--- src/Game/Map.js
+++ src/Game/Map.js
@@ -7,18 +7,39 @@ const simplex = new noise();
 import type State from 'Game/state'
 
 import Rect from 'Game/Rect'
+import Point from 'Game/Point'
 
 import Grid from 'Game/Type/Grid'
 export default function genMap(state:State){
 	let r = new Rect({x:1,y:1}, {x: 3, y:100})
 	fill(state, 'ROAD', r);
-	drawRoom(state, new Rect({x:4,y:4}, {x: 20, y:100}))
+	cityBlock(state, new Point({x:4, y:4}))
 }
 
+function cityBlock(state:State, offset:Point){
+
+	let mid = offset.x+10+Math.floor(Math.random()*10)
+	let end = mid+10+Math.floor(Math.random()*10)
+	let dist = 0
+
+	for(let i = 0; i< 10; i++){
+
+		let len = 5+Math.floor(Math.random()*8)
+		drawRoom(state, new Rect(offset.y, offset.x+mid, offset.y + len, offset.x));
+
+		offset.y+= len
+		// let otherCorner = offset.add({
+		// 	x:10+Math.floor(Math.random()*10),
+		// 	y:5+Math.floor(Math.random()*8)
+		// })
+		// drawRoom(state, new Rect(offset, otherCorner))
+		// offset = new Point({x:offset.x, y:otherCorner.y})
+
+	}
+}
 
 function fill(state:State, type: string, rect:Rect){
 	rect.units.forEach(b => {
-		// console.log(b)
 		state.grid.addNode(b.x, b.y, new Grid({type}))
 	})
 }
