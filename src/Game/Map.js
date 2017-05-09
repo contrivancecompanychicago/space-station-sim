6906f6eaaafd4b3939efcc9a054f81431cbd75da
digi <james@digijin.com>
2017-05-09 20:43:41 +1000

    making blocks

--- src/Game/Map.js
+++ src/Game/Map.js
@@ -10,54 +10,68 @@ import Rect from 'Game/Rect'
 import Point from 'Game/Point'
 
 import Grid from 'Game/Type/Grid'
-export default function genMap(state:State){
-	let r = new Rect({x:1,y:1}, {x: 3, y:100})
+export default function genMap(state: State) {
+	let r = new Rect({ x: 1, y: 1 }, { x: 3, y: 100 })
 	fill(state, 'ROAD', r);
-	cityBlock(state, new Point({x:4, y:4}))
+	cityBlock(state, new Point({ x: 4, y: 4 }))
 }
 
-function cityBlock(state:State, offset:Point){
+function cityBlock(state: State, offset: Point) {
 
-	let mid = offset.x+10+Math.floor(Math.random()*10)
-	let end = mid+10+Math.floor(Math.random()*10)
+	let padding = 2;
+
+	let lens = []
+	for (let i = 0; i < 5; i++) {
+		lens.push(5 + Math.floor(Math.random() * 4))
+	}
+	let height = lens.reduce((a,b) => {return a+b}, 0);
+
+	let mid = offset.x + 6 + Math.floor(Math.random() * 4)
+	let end = mid + 6 + Math.floor(Math.random() * 4)
 	let dist = 0
 
-	for(let i = 0; i< 10; i++){
 
-		let len = 5+Math.floor(Math.random()*8)
-		drawRoom(state, new Rect(offset.y, offset.x+mid, offset.y + len, offset.x));
+	fill(state, 'FLOOR', new Rect({
+		t:offset.y,
+		r:padding+offset.x+end+padding,
+		b:padding+offset.y+height+padding,
+		l:offset.x}))
 
-		offset.y+= len
-		// let otherCorner = offset.add({
-		// 	x:10+Math.floor(Math.random()*10),
-		// 	y:5+Math.floor(Math.random()*8)
-		// })
-		// drawRoom(state, new Rect(offset, otherCorner))
-		// offset = new Point({x:offset.x, y:otherCorner.y})
+	
+	offset.x += padding
+	offset.y += padding
 
-	}
-}
 
-function fill(state:State, type: string, rect:Rect){
-	rect.units.forEach(b => {
-		state.grid.addNode(b.x, b.y, new Grid({type}))
+
+	lens.forEach(len => {
+
+		drawRoom(state, new Rect(offset.y, offset.x + mid, offset.y + len, offset.x));
+		drawRoom(state, new Rect(offset.y, offset.x + end, offset.y + len, offset.x + mid));
+
+		offset.y += len
 	})
 }
-function drawRoom(state:State, rect:Rect){
+
+function fill(state: State, type: string, rect: Rect) {
+	rect.units.forEach(b => {
+		state.grid.addNode(b.x, b.y, new Grid({ type }))
+	})
+}
+function drawRoom(state: State, rect: Rect) {
 	fill(state, 'WALLTEST', rect)
-	rect = rect.add({t:1,r: -1,b: -1,l: 1})
+	rect = rect.add({ t: 1, r: -1, b: -1, l: 1 })
 	fill(state, 'FLOOR', rect)
 }
 
-function drawSimplex(state:State, wide:number, high:number):State{
-	for(let x = 0; x< wide; x++){
-		for(let y = 0; y< high; y++){
-			let n = simplex.noise2D(x/10, y/10);
+function drawSimplex(state: State, wide: number, high: number): State {
+	for (let x = 0; x < wide; x++) {
+		for (let y = 0; y < high; y++) {
+			let n = simplex.noise2D(x / 10, y / 10);
 
-			let gridParams = {type: 'FLOOR'};
-			if(Math.abs(n) < 0.25) gridParams.type = 'WALLTEST'
-			state.grid.addNode(x,y, new Grid(gridParams))
-			
+			let gridParams = { type: 'FLOOR' };
+			if (Math.abs(n) < 0.25) gridParams.type = 'WALLTEST'
+			state.grid.addNode(x, y, new Grid(gridParams))
+
 
 		}
 	}
